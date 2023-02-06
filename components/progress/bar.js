bc276b009fe543ece08d9a8772bd030b435f3e33
Sroda Mensah <mensahsrora36@gmail.com>
2023-02-06 17:05:53 +0000

    Progress

--- components/progress/bar.js
+++ components/progress/bar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Bar({ animationDuration, progress }) {
     return (
         <div
-            className='bg-[#92ec92] h-1 w-full left-0, top-0 fixed z-50'
+            className='bg-[#F2F3F2] h-1 w-full left-0, top-0 fixed z-50'
             style={{
                 marginLeft: `${(-1 + progress) * 100}%`,
                 transition: `margin-left ${animationDuration}ms linear`
